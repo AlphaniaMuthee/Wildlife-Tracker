2126a20dad59ad6642731eac0b5b48bb344dc82a
AlphaniaMuthee <walphania2@gmail.com>
2020-05-03 02:10:13 +0300

    add tests for instantiating Sightings with locationa and animal Ids

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -7,5 +7,22 @@ public class Sightings {
         this.animalId = animalId;
     }
 
+    public String getLocation() {
+        return Location;
+    }
 
+    public int getAnimalId() {
+        return animalId;
+    }
+
+    @Override
+    public boolean equals(Object otherSightings){
+        if (!(otherSightings instanceof Sightings)) {
+            return false;
+        } else {
+            Sightings newSightings = (Sightings) otherSightings;
+            return this.getLocation().equals(newSightings.getLocation()) &&
+                    this.getAnimalId() == newSightings.getAnimalId();
+        }
+    }
 }
--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -8,6 +8,29 @@ public class SightingsTest {
     @Rule
     public DatabaseRule database = new DatabaseRule();
 
+    @Test
+    public void sightings_InstantiatesCorrectly_true() {
+        Sightings testSightings = new Sightings ("NE Quadrant", 1);
+        assertEquals(true, testSightings instanceof Sightings );
+    }
 
+    @Test
+    public void sightings_instantiateWithLocation_String(){
+        Sightings testSightings = new Sightings ("NE Quadrant", 1);
+        assertEquals("NE Quadrant", testSightings.getLocation());
+    }
+
+    @Test
+    public void sightings_instantiateWithAnimalId_int(){
+        Sightings testSightings = new Sightings("NE Quadrant", 1);
+        assertEquals(1, testSightings.getAnimalId());
+    }
+
+    @Test
+    public void equals_returnsTrueIfLocationAndAnimalIdAreSAme_true() {
+        Sightings testSightings = new Sightings ("NE Quadrant", 1);
+        Sightings anotherSightings = new Sightings ("NE Quadrant", 1);
+        assertTrue(testSightings.equals(anotherSightings));
+    }
 
 }
