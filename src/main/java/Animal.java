a849c68316ec4c502ae17233dbf1dcceefdb3236
AlphaniaMuthee <walphania2@gmail.com>
2020-05-02 19:16:18 +0300

    add animal instantiate tes

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,5 +1,17 @@
 public class Animal {
-    public Animal(){
+    private String name;
+    private int id;
 
+    public Animal(String name, int id) {
+        this.name = name;
+        id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
     }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+
+    @Test
+    public void animal_instantiatesCorrectly_true() {
+        Animal testAnimal = new Animal("Monkey", 1);
+        assertEquals(true, testAnimal instanceof Animal);
+    }
 
 }
