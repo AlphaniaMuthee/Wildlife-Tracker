26462c30c11161f0368b0556c16949ae4e7f3270
AlphaniaMuthee <walphania2@gmail.com>
2020-05-03 01:54:50 +0300

    add constructor methods for Sightings file

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -1,3 +1,11 @@
 public class Sightings {
+    private String Location;
+    private int animalId;
+
+    public Sightings(String location, int animalId) {
+        Location = location;
+        this.animalId = animalId;
+    }
+
 
 }
--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -1,5 +1,13 @@
+import org.junit.*;
+import org.sql2o.*;
+
 import static org.junit.Assert.*;
 
 public class SightingsTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+
 
 }
