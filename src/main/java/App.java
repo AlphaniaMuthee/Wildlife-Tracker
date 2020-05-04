22314ff3a71eb3b2f7e0b3a3bd52fb0c347452f2
AlphaniaMuthee <walphania2@gmail.com>
2020-05-04 12:33:15 +0300

    add animals-list.hbs to templates to view animals added to the app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -35,62 +35,10 @@ public class App {
                         "</html>"
         );
 
-        get("/endangered/", (request, response) ->
-                "<!DOCTYPE html>" +
-                        "<html>" +
-                        "<head>" +
-                        "<title>Endangered animals entry</title>" +
-                        "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                        "</head>" +
-                        "<body>" +
-                        "<h1>ENDANGERED ANIMALS ENTRY FORM</h1>" +
-                        "<form action=\"/endangered/list\" method=\"post\">"+
+        get("/endangered/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "endangered.hbs");
+        }, new HandlebarsTemplateEngine());
 
-                        "<div class=\"form-group\">" +
-                        "<input type=\"text\" id=\"name\" placeholder=\"Name of Animal spotted\" >" +
-                        "</div>" +
-
-                        "<div class=\"form-group\">" +
-                        "<label for=health>What is the animal's state of health</label>" +
-                        "<select name=health>"+
-                        "<option>Make a selection</option>"+
-                        "<option>Ill</option>"+
-                        "<option>Okay</option>"+
-                        "<option> Healthy</option>" +
-                        "</select>" +
-                        "</div>" +
-
-                        "<div class=\"form-group\">" +
-                        "<label for=age>How old is the animal?</label>" +
-                        "<select name=age>"+
-                        "<option>Make a selection</option>"+
-                        "<option>Newborn</option>"+
-                        "<option>Young</option>"+
-                        "<option>Adult</option>" +
-                        "</select>" +
-                        "</div>" +
-
-                        "<div class=\"form-group\">" +
-                        "<label for=location>Location where animal was spotted</label>" +
-                        "<select name=location>"+
-                        "<option>Make a selection</option>"+
-                        "<option>NE Quadrant</option>"+
-                        "<option>SE Quadrant</option>"+
-                        "<option>SW Quadrant</option>" +
-                        "<option>NW Quadrant</option>" +
-                        "</select>" +
-                        "</div>" +
-
-                        "<div class=\"form-group\">" +
-                        "<input type=\"text\" id=\"rangername\" placeholder=\"Name of the ranger in charge of the sighting\" >" +
-                        "</div>" +
-
-                        "<button type=\"submit\" class=\"btn btn-success\">Add Animal</button>" +
-
-                        "</form>" +
-                        "</body>" +
-                        "</html>"
-        );
         get("/endangered/list/", (request, response) ->
                 "<!DOCTYPE html>" +
                         "<html>" +
