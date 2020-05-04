fdd421ae0f050e82b3eb4c68f2fc5e96ee3e5e96
AlphaniaMuthee <walphania2@gmail.com>
2020-05-04 23:45:05 +0300

    add pom.xml file to project

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,10 @@
 import org.sql2o.*;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "alphania", "2020");
+    //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "alphania", "2020");;
+    public static Sql2o sql2o = new Sql2o("jdbc:postgress://ec2-52-207-25-133.compute-1.amazonaws.com:5432/dqhrjghhngdpq", "jpnwpfidtifdfl", "8208486c6a1d7b4fc4aab658e3571252200dd0979e447388da3c1651ddf49b18");
 }
+
+
+
+//postgres://jpnwpfidtifdfl:8208486c6a1d7b4fc4aab658e3571252200dd0979e447388da3c1651ddf49b18@ec2-52-207-25-133.compute-1.amazonaws.com:5432/dqhrjghhngdpq
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -4,7 +4,7 @@ import org.sql2o.*;
 public class DatabaseRule extends ExternalResource {
     @Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "alphania", "2020");
+        DB.sql2o = new Sql2o("jdbc:postgress://ec2-52-207-25-133.compute-1.amazonaws.com:5432/dqhrjghhngdpq", "jpnwpfidtifdfl", "8208486c6a1d7b4fc4aab658e3571252200dd0979e447388da3c1651ddf49b18");
     }
 
     @Override
